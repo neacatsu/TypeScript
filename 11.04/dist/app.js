6360c811bcb92582427ebb0fabde8d73c3cbe9f4
neacatsu <wiktoria.wierzbicka166@gmail.com>
2021-04-25 10:58:47 +0200

    add solution

--- 11.04/dist/app.js
+++ 11.04/dist/app.js
@@ -10,38 +10,80 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 class WeatherApp {
     constructor() {
         this.cityArray = [];
-        this.key = "90a9b5280a6424dc8d91244921ae291e";
+        this.storageArray = [];
+        this.key = "75f816acc7b275d8f6708a1c05f5a959";
+        this.getWeather = (cityName) => __awaiter(this, void 0, void 0, function* () {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${this.key}`)
+                .then(res => res.json())
+                .then(data => this.showWeather(data, cityName))
+                .catch(err => console.error(err));
+        });
         this.startApp();
     }
     startApp() {
-        this.getWeather("Kraków", this.key);
+        this.takeStorage();
+        window.setInterval(() => this.takeStorage(), 120000);
+        this.takeCity();
     }
-    getWeather(cityName, APIKey) {
-        return __awaiter(this, void 0, void 0, function* () {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`)
-                .then(res => res.json())
-                .then(data => this.handleWeatherData(data))
-                .catch(err => console.error(err));
+    takeStorage() {
+        if (window.localStorage.getItem("cities")) {
+            const storagedItem = window.localStorage.getItem("cities");
+            window.localStorage.removeItem("cities");
+            this.storageArray = JSON.parse(storagedItem);
+            const root = document.querySelector(".root");
+            root.innerHTML = "";
+            this.storageArray.forEach((element) => {
+                this.getWeather(element);
+            });
+        }
+    }
+    takeCity() {
+        const button = document.querySelector("button");
+        button.addEventListener("click", (e) => this.takeCityInput(e));
+    }
+    takeCityInput(e) {
+        e.preventDefault();
+        const input = document.querySelector("input");
+        const boxes = document.querySelectorAll(".box");
+        let setData = true;
+        boxes.forEach((element) => {
+            if (element.firstChild.firstChild.nodeValue == input.value.normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
+                setData = false;
         });
+        setData && this.getWeather(input.value);
     }
-    handleWeatherData(data) {
-        this.showWeather(data);
-    }
-    showWeather(data) {
+    showWeather(data, cityName) {
+        const singleCity = this.cityArray.find(element => element === cityName);
+        singleCity === undefined && this.cityArray.push(data.name);
+        window.localStorage.setItem("cities", JSON.stringify(this.cityArray));
         const root = document.querySelector(".root");
         const div = document.createElement("div");
         const h2 = document.createElement("h2");
         const temp = document.createElement("span");
         const pressure = document.createElement("span");
         const humidity = document.createElement("span");
+        const innerDiv = document.createElement("div");
+        const rightSide = document.createElement("div");
+        const icon = document.createElement("img");
+        temp.setAttribute("class", "temp");
+        pressure.setAttribute("class", "pressure");
+        innerDiv.setAttribute("class", "innerDiv");
+        humidity.setAttribute("class", "humidity");
+        rightSide.setAttribute("class", "rightSide");
+        div.setAttribute("class", "box");
+        icon.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+        icon.setAttribute("class", "img");
         h2.innerText = data.name;
-        temp.innerText = data.main.temp;
-        pressure.innerText = data.main.pressure;
-        humidity.innerText = data.main.humidity;
+        temp.innerText = `${Math.round(data.main.temp)} °C`;
+        pressure.innerText = `Ciśnienie ${data.main.pressure}hPa`;
+        humidity.innerText = `Wilgotność ${data.main.humidity}%`;
+        rightSide.appendChild(pressure);
+        rightSide.appendChild(humidity);
+        innerDiv.appendChild(temp);
+        innerDiv.appendChild(rightSide);
         div.appendChild(h2);
-        div.appendChild(temp);
-        div.appendChild(pressure);
-        div.appendChild(humidity);
+        div.appendChild(icon);
+        div.appendChild(innerDiv);
         root.appendChild(div);
     }
 }
--- 11.04/src/app.ts
+++ 11.04/src/app.ts
@@ -1,48 +1,95 @@
-class WeatherApp{
-    cityArray: string[] = []
-    city: string
-    key: string="90a9b5280a6424dc8d91244921ae291e"
+class WeatherApp {
+    cityArray: string[] = [];
+    storageArray: string[] = [];
+    city: string;
+    key: string = "75f816acc7b275d8f6708a1c05f5a959";
 
-    constructor(){
-        this.startApp()
+    constructor() {
+        this.startApp();
     }
 
-    startApp(){
-        this.getWeather("Kraków",this.key)
-
+    startApp(): void {
+        this.takeStorage();
+        window.setInterval(() => this.takeStorage(), 120000);
+        this.takeCity();
     }
 
-    async getWeather(cityName: string, APIKey: string) {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`)
+    getWeather = async (cityName: string): Promise<void> => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${this.key}`)
             .then(res => res.json())
-            .then(data => this.handleWeatherData(data))
-            .catch(err => console.error(err))
+            .then(data => this.showWeather(data, cityName))
+            .catch(err => console.error(err));
+    }
+    
+    takeStorage(): void {
+        if(window.localStorage.getItem("cities")) {
+            const storagedItem: string = window.localStorage.getItem("cities");
+            window.localStorage.removeItem("cities");
+            this.storageArray = JSON.parse(storagedItem);
+            const root: HTMLDivElement = document.querySelector(".root");
+            root.innerHTML = "";
+            this.storageArray.forEach((element: string) => {
+                this.getWeather(element);
+            })
+        }
     }
 
-    handleWeatherData(data){
-        this.showWeather(data)
+    takeCity(): void {
+        const button: HTMLButtonElement = document.querySelector("button");
+        button.addEventListener("click", (e: Event) => this.takeCityInput(e));
     }
 
-    showWeather(data){
-        const root= document.querySelector(".root");
-        const div= document.createElement("div");
-        const h2= document.createElement("h2");
-        const temp= document.createElement("span");
-        const pressure= document.createElement("span");
-        const humidity= document.createElement("span");
+    takeCityInput(e: Event): void{
+        e.preventDefault();
 
-        h2.innerText=data.name;
-        temp.innerText=data.main.temp;
-        pressure.innerText=data.main.pressure;
-        humidity.innerText=data.main.humidity;
+        const input: HTMLInputElement = document.querySelector("input");
+        const boxes: NodeListOf<HTMLDivElement> = document.querySelectorAll(".box");
+        let setData: boolean = true;
+        boxes.forEach((element: HTMLDivElement) => {
+            if(element.firstChild.firstChild.nodeValue == input.value.normalize("NFD").replace(/[\u0300-\u036f]/g, "")) setData = false;
+        })
+        setData && this.getWeather(input.value);
+    }
+
+    showWeather(data: any, cityName: string){
+        const singleCity: string = this.cityArray.find(element => element === cityName);
+        singleCity === undefined && this.cityArray.push(data.name);
+        window.localStorage.setItem("cities", JSON.stringify(this.cityArray));
+
+        const root: HTMLDivElement = document.querySelector(".root");
+        const div: HTMLDivElement = document.createElement("div");
+        const h2: HTMLElement = document.createElement("h2");
+        const temp: HTMLSpanElement = document.createElement("span");
+        const pressure: HTMLSpanElement = document.createElement("span");
+        const humidity: HTMLSpanElement = document.createElement("span");
+        const innerDiv: HTMLDivElement = document.createElement("div");
+        const rightSide: HTMLDivElement = document.createElement("div");
+        const icon: HTMLDivElement = document.createElement("img");
+
+        temp.setAttribute("class", "temp");
+        pressure.setAttribute("class", "pressure");
+        innerDiv.setAttribute("class", "innerDiv");
+        humidity.setAttribute("class", "humidity");
+        rightSide.setAttribute("class", "rightSide");
+        div.setAttribute("class","box");
+        icon.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+        icon.setAttribute("class","img");
+
+        h2.innerText = data.name;
+        temp.innerText = `${Math.round(data.main.temp)} °C`;
+        pressure.innerText = `Ciśnienie ${data.main.pressure}hPa`;
+        humidity.innerText = `Wilgotność ${data.main.humidity}%`;
+        
+        rightSide.appendChild(pressure);
+        rightSide.appendChild(humidity);
+        innerDiv.appendChild(temp);
+        innerDiv.appendChild(rightSide);
 
         div.appendChild(h2);
-        div.appendChild(temp);
-        div.appendChild(pressure);
-        div.appendChild(humidity);
+        div.appendChild(icon);
+        div.appendChild(innerDiv);
+
         root.appendChild(div);
     }
-
-
 }
-const weatherApp= new WeatherApp()
+const weatherApp= new WeatherApp();
